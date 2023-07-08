a998c57937f3016e7cd1f886ee3dd6669ae1f5ae
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2023-07-08 14:39:55 +0330

    command is updated

--- BaseUser/management/commands/createsuperuser.py
+++ BaseUser/management/commands/createsuperuser.py
@@ -17,8 +17,9 @@ class Command(BaseCommand):
         password = input('password: ')
         mobile_number = input('mobile number: ')
 
-        admin_user = User(gender='male', first_name=first_name, last_name=last_name, mobile_number=mobile_number, is_active=True,
-             is_staff=True, is_superuser=True,
-             )
+        admin_user = User(gender='male', first_name=first_name, last_name=last_name, mobile_number=mobile_number,
+                          is_active=True,
+                          is_staff=True, is_superuser=True, username=mobile_number,
+                          )
         admin_user.set_password(password)
         admin_user.save()
