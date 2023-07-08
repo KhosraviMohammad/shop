70ac529c3df4f98b08984e306316524ec4923fc5
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2023-07-08 21:18:30 +0330

    category model is created

--- Product/models.py
+++ Product/models.py
@@ -18,6 +18,14 @@ class Product(user_models.BaseFieldsModel):
     image = models.ImageField(upload_to=product_image_directory_path, blank=True, null=True, verbose_name='تصویر')
     price = models.IntegerField(default=0, blank=True, verbose_name='قیمت')
     is_available = models.BooleanField(default=False, blank=True, )
+    categories = models.ManyToManyField('Product.Category', blank=True, related_name='product_set', verbose_name='دسته بندی')
+
+    def __str__(self):
+        return self.name
+
+
+class Category(user_models.BaseFieldsModel):
+    name = models.CharField(max_length=150, unique=True, verbose_name='نام')
 
     def __str__(self):
         return self.name
