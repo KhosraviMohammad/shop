f7b426f9e5e2aca529d00bc060c0842b00bec6b7
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2023-07-08 11:07:31 +0330

    some api links are added

--- Setting/urls.py
+++ Setting/urls.py
@@ -1,11 +1,17 @@
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenBlacklistView,
-)
-
 from django.urls import path
 
 
+from Setting.views import (
+    api_root, api_v1_root, account_api_v1_root
+)
+
+
 urlpatterns = [
+    path('', api_root, name='api_root'),
+    path('api/v1/', api_v1_root, name='api_v1_root'),
+
+    # api v1 start
+    path('api/v1/account', account_api_v1_root, name='account_api_v1_root'),
+    # api v1 end
+
 ]
--- Setting/views.py
+++ Setting/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'apis': {
+            'api v1': reverse('api_v1_root', request=request, format=format)
+        }
+    })
+
+
+@api_view(['GET'])
+def api_v1_root(request, format=None):
+    return Response({
+        'apis': {
+            'account api': reverse('account_api_v1_root', request=request, format=format),
+        }
+    })
+
+
+@api_view(['GET'])
+def account_api_v1_root(request, format=None):
+    return Response({
+        'apis': {
+            'token': reverse('token_obtain_pair', request=request, format=format),
+            'token refresh': reverse('token_refresh', request=request, format=format),
+            'token blacklist': reverse('token_blacklist', request=request, format=format),
+        }
+    })
+
+
+
+
+
