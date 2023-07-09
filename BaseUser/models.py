b927bf07f5d9c3ce8b409b5dfc523e6e3e0028dc
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2023-07-09 15:25:45 +0330

    gettext_lazy function putted over strings to translate

--- BaseUser/models.py
+++ BaseUser/models.py
@@ -7,7 +7,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, UserManager, PermissionsMixin
 from django.core.validators import RegexValidator
 from django.conf import settings
-
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
@@ -36,8 +36,8 @@ class BaseFieldsModel(models.Model):
     زمان ایجاد و بروزرسانی
 
     """
-    create_date = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
-    update_date = models.DateTimeField(auto_now=True, verbose_name='تاریخ ویرایشی')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name=_('تاریخ ایجاد'))
+    update_date = models.DateTimeField(auto_now=True, verbose_name=_('تاریخ ویرایشی'))
 
     class Meta:
         abstract = True
@@ -51,9 +51,9 @@ class BaseFieldsModel(models.Model):
 
 class BaseUserFieldModel(BaseFieldsModel):
     created_by = models.ForeignKey('BaseUser.User', on_delete=models.DO_NOTHING, blank=True, null=True,
-                                   verbose_name='سازنده', related_name='created_%(class)ss')
+                                   verbose_name=_('سازنده'), related_name='created_%(class)ss')
     updated_by = models.ForeignKey('BaseUser.User', on_delete=models.DO_NOTHING, blank=True, null=True,
-                                   verbose_name='آخرین ویراستار', related_name='updated_%(class)ss')
+                                   verbose_name=_('آخرین ویراستار'), related_name='updated_%(class)ss')
 
     class Meta:
         abstract = True
@@ -79,18 +79,18 @@ class User(AbstractBaseUser, BaseFieldsModel, PermissionsMixin):
         ('female', 'مونث'),
         ('male', 'مذکر'),
     )
-    username = models.CharField(max_length=150, unique=True, verbose_name='نام کاربری')
-    first_name = models.CharField(verbose_name='نام', max_length=30)
-    gender = models.CharField(choices=GENDER_SELECT, verbose_name='جنسیت', max_length=20)
-    last_name = models.CharField(verbose_name='نام خانوادگی', max_length=30)
+    username = models.CharField(max_length=150, unique=True, verbose_name=_('نام کاربری'))
+    first_name = models.CharField(verbose_name=_('نام'), max_length=30)
+    gender = models.CharField(choices=GENDER_SELECT, verbose_name=_('جنسیت'), max_length=20)
+    last_name = models.CharField(verbose_name=_('نام خانوادگی'), max_length=30)
     mobile_number = models.CharField(max_length=11, unique=True,
                                      validators=[RegexValidator(re.compile(r'^[0-9]{11}$'))],
-                                     verbose_name='شماره همراه',
-                                     help_text='یازده رقمی بدون خط تیره. نمونه: 09121328462')
+                                     verbose_name=_('شماره همراه'),
+                                     help_text=_('یازده رقمی بدون خط تیره. نمونه: 09121328462'))
     avatar = models.ImageField(upload_to=avatar_directory_path, blank=True, null=True)
-    is_active = models.BooleanField(verbose_name='وضعیت', default=True)
-    is_staff = models.BooleanField(verbose_name='وضعیت کارمندی', default=False)
-    is_superuser = models.BooleanField(verbose_name='ابر کاربر', default=False)
+    is_active = models.BooleanField(verbose_name=_('وضعیت'), default=True)
+    is_staff = models.BooleanField(verbose_name=_('وضعیت کارمندی'), default=False)
+    is_superuser = models.BooleanField(verbose_name=_('ابر کاربر'), default=False)
 
     USERNAME_FIELD = 'mobile_number'
     REQUIRED_FIELDS = []
--- Product/models.py
+++ Product/models.py
@@ -3,7 +3,7 @@ import os
 from django.db import models
 
 from BaseUser import models as user_models
-
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
@@ -14,18 +14,18 @@ def product_image_directory_path(instance, filename):
 
 
 class Product(user_models.BaseUserFieldModel):
-    name = models.CharField(max_length=150, verbose_name='نام محصول')
-    image = models.ImageField(upload_to=product_image_directory_path, blank=True, null=True, verbose_name='تصویر')
-    price = models.IntegerField(default=0, blank=True, verbose_name='قیمت')
+    name = models.CharField(max_length=150, verbose_name=_('نام محصول'))
+    image = models.ImageField(upload_to=product_image_directory_path, blank=True, null=True, verbose_name=_('تصویر'))
+    price = models.IntegerField(default=0, blank=True, verbose_name=_('قیمت'))
     is_available = models.BooleanField(default=False, blank=True, )
-    categories = models.ManyToManyField('Product.Category', blank=True, related_name='product_set', verbose_name='دسته بندی')
+    categories = models.ManyToManyField('Product.Category', blank=True, related_name='product_set', verbose_name=_('دسته بندی'))
 
     def __str__(self):
         return self.name
 
 
 class Category(user_models.BaseUserFieldModel):
-    name = models.CharField(max_length=150, unique=True, verbose_name='نام')
+    name = models.CharField(max_length=150, unique=True, verbose_name=_('نام'))
 
     def __str__(self):
         return self.name
