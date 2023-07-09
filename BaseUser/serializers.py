a7571d320c1ae7bb8165e84c77b6d67bc6041dfb
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2023-07-09 13:56:33 +0330

    CustomTokenObtainPairSerializer is created

--- BaseUser/serializers.py
+++ BaseUser/serializers.py
@@ -1,8 +1,12 @@
 from django.db import transaction
 
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import update_last_login
 
-from BaseUser.models import User
+from BaseUser.models import User, BlackListedAccessToken, OutstandingAccessToken
+from rest_framework_simplejwt.settings import api_settings
+from rest_framework_simplejwt.serializers import TokenBlacklistSerializer, TokenObtainSerializer
 
 
 class UserRegisterSerializer(serializers.HyperlinkedModelSerializer):
@@ -18,3 +22,22 @@ class UserRegisterSerializer(serializers.HyperlinkedModelSerializer):
             user.set_password(password)
             user.save()
         return user
+
+
+class CustomTokenObtainPairSerializer(TokenObtainSerializer):
+    token_class = RefreshToken
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        refresh = self.get_token(self.user)
+
+        data["refresh"] = str(refresh)
+        data["access"] = str(refresh.access_token)
+        outstanding_access_token = OutstandingAccessToken(token=data["access"], user=self.context.get('user'))
+        outstanding_access_token.save()
+
+        if api_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, self.user)
+
+        return data
