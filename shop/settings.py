9a20d5ba84e417c52fa3a7d16e974bb9f455aa90
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2023-07-13 09:54:48 +0330

    change db

--- shop/settings.py
+++ shop/settings.py
@@ -94,27 +94,36 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql_psycopg2'),
+        'NAME': os.getenv('DB_NAME', 'shop'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'amir'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', 5432),
+    },
+}
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.mysql'),
 #         'NAME': os.getenv('DB_NAME', 'shop'),
-#         'USER': os.getenv('DB_USER', 'root1'),
+#         'USER': os.getenv('DB_USER', 'root'),
 #         'PASSWORD': os.getenv('DB_PASSWORD', 'amir'),
 #         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
 #         'PORT': os.getenv('DB_PORT', '3306'),
 #     },
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.mysql'),
-        'NAME': os.getenv('DB_NAME', 'shop'),
-        'USER': os.getenv('DB_USER', 'root1'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'amir'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '3306'),
-    },
-}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
