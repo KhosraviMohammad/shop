30782edc2aac834696d88e421af4e3f3cf588ca4
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2023-07-15 09:28:47 +0330

    models are registered to admin panel

--- Product/admin.py
+++ Product/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from Product.models import Category, Product
+
 
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.save(user=request.user)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.save(user=request.user)
